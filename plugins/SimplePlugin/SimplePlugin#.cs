7593cb390c1be53b49443a0f55dd6e61adb1eaf9
kshahar <kshahar@gmail.com>
2009-01-09 19:46:49 +0000

    SimplePlugin setup
    --HG--
    extra : convert_revision : svn%3A042b8ba0-128a-459c-b0f4-fb341159ecef/trunk%4043

---
+++ plugins/SimplePlugin/SimplePlugin#.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace LaunchySharp
+{
+	public class SimplePlugin: IPlugin
+	{
+		private LaunchySharp.IPluginHost m_pluginHost = null;
+		private LaunchySharp.ICatItemFactory m_catItemFactory = null;
+		private uint m_id = 0;
+		private string m_name = "SimplePlugin#";
+	
+		public void init(LaunchySharp.IPluginHost pluginHost)
+		{
+			m_pluginHost = pluginHost;
+			if (m_pluginHost != null) {
+				m_catItemFactory = m_pluginHost.catItemFactory();
+			}
+			m_id = m_pluginHost.hash(m_name);
+		}
+		
+		public uint getID()
+		{
+			return m_id;
+		}
+		
+		public string getName()
+		{
+			return m_name;
+		}
+		
+		public void getLabels(List<IInputData> inputDataList)
+		{
+		}
+		
+		public void getResults(List<IInputData> inputDataList, List<ICatItem> resultsList)
+		{
+			string text = inputDataList[0].getText();
+			resultsList.Add( m_catItemFactory.createCatItem( 
+				text, "SimplePlugin: " + text, getID(), getName()) );
+		}
+		
+		public void getCatalog(List<ICatItem> catalogItems)
+		{
+		}
+		
+		public void launchItem(List<IInputData> inputDataList, ICatItem item)
+		{
+			ICatItem catItem = 
+				inputDataList[inputDataList.Count-1].getTopResult();
+			MessageBox.Show("I was asked to launch: " + item.getFullPath());
+		}
+	}
+}
---
+++ plugins/SimplePlugin/make_SimplePlugin#.bat
@@ -0,0 +1,3 @@
+call "D:\Program Files\Microsoft Visual Studio 8\VC\vcvarsall.bat"
+csc /t:library /reference:"C:\Program Files\Launchy\Launchy#API.dll" SimplePlugin#.cs 
+pause
