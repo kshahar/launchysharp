4e6562fb4edba267a408adcc48f891dab3f64353
kshahar <kshahar@gmail.com>
2009-01-09 10:23:38 +0000

    Files reorganization
    --HG--
    extra : convert_revision : svn%3A042b8ba0-128a-459c-b0f4-fb341159ecef/trunk%4023

--- src/LaunchySharp/LaunchySharpTest/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Precompiled.h"
-#include <gmock/gmock.h>
-#include <gtest/gtest.h>
-
-int main(int argc, char* argv[]) 
-{
-	testing::InitGoogleMock(&argc, argv);
-	return RUN_ALL_TESTS();
-}
